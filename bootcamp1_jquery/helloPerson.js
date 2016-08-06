2fd5bf4d25beb0cc746702b75f1cc9fd12fdf610
Jack-Wong94 <lwon229@aucklanduni.ac.nz>
2016-08-06 13:49:30 +1200

    Add project file

---
+++ bootcamp1_jquery/helloPerson.js
@@ -0,0 +1,7 @@
+function greeter(person) {
+    return "Hello, " + person;
+}
+
+var user = "What is fddfsdsaf///";
+
+document.body.innerHTML = greeter(user);
---
+++ bootcamp1_jquery/helloPerson.ts
@@ -0,0 +1,7 @@
+function greeter(person: string) {
+    return "Hello, " + person;
+}
+
+var user = "What idsfs g/";
+
+document.body.innerHTML = greeter(user);       
